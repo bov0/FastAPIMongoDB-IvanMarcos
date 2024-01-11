ba99d16f93fece70853a178d57a8ee7c7f19e156
bov0 <ibay2002@gmail.com>
2024-01-11 12:25:28 +0100

    first commit

--- models/animal.py
+++ models/animal.py
@@ -3,9 +3,8 @@ from pydantic import BaseModel
 from datetime import date
 
 class Animal(BaseModel):
-    id_animal: Optional[int]
     nombre_animal: str
     fecha_nacimiento: date
     edad: int
-    id_especie: int
-    id_habitat: int
+    especie: str
+    habitat: str
--- schemas/animal.py
+++ schemas/animal.py
@@ -1,11 +1,11 @@
 def animalEntity(item) -> dict:
     return {
-        "id_animal": item["id_animal"],
+        "id_animal": str(item["_id"]),
         "nombre_animal": item["nombre_animal"],
         "fecha_nacimiento": item["fecha_nacimiento"],
         "edad": item["edad"],
-        "id_especie": item["id_especie"],
-        "id_habitat": item["id_habitat"]
+        "especie": item["especie"],
+        "habitat": item["habitat"]
     }
 
 def animalesEntity(entity) -> list:
